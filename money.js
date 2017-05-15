3813ec00f8b6cd6fda6d3d60ebeceffa84cedb0b
Carlos Torres <8ctorres@stpatrick.org>
2017-05-14 20:58:33 -0500

    Minor error solving

--- money.js
+++ money.js
@@ -3,28 +3,28 @@
 
 function permutations(actual,objetivo){
         if (actual==objetivo){
-            Return 1
+            return 1;
           }
         else if (actual>objetivo){
-            Return 0
+            return 0;
             }
         else{
-            Return permutations((actual+1), objetivo) + permutations((actual+5), objetivo) + permutations((actual+10), objetivo) + permutations((actual+25), objetivo)
+            return permutations((actual+1), objetivo) + permutations((actual+5), objetivo) + permutations((actual+10), objetivo) + permutations((actual+25), objetivo);
           }
         }
 
 // Code to run the function
 
-        console.log("Running...")
-        For (let i=1; i<=100;i++){
-            console.log("Goal for this cycle: " + i)
-//            Console.log("Starting at: " & DateAndTime.Now & "." & DateAndTime.Now.Millisecond)
-            console.log("")
-           console.log(permutations(0, i))
-            console.log("")
-            console.log("Done")
-  //          Console.log("Completed at: " & DateAndTime.Now & "." & DateAndTime.Now.Millisecond)
-            console.log("")
-            console.log("-------")
-            console.log("")
+        console.log("Running...");
+        for(let i=1; i<=100;i++){
+            console.log("Goal for this cycle: " + i);
+  //          console.log("Starting at: " & DateAndTime.Now & "." & DateAndTime.Now.Millisecond);
+            console.log("");
+            console.log(permutations(0, i));
+            console.log("");
+            console.log("Done");
+  //          console.log("Completed at: " & DateAndTime.Now & "." & DateAndTime.Now.Millisecond);
+            console.log("");
+            console.log("-------");
+            console.log("");
         }
--- path.js
+++ path.js
@@ -4,41 +4,41 @@
 
 function buildPath(p,n){
 // console.log("Entering buildPath...")
-let path = []
+let path = [];
 for (let i =0; i<n;i++){
   let r = Math.random();
 //  console.log(r)
   if (r<=p){
-     path[i]=false
+     path[i]=false;
 //    console.log("posicion asignada false")
   }
     else{
-      path[i]=true
+      path[i]=true;
 //    console.log("posicion asignada true")
     }
    }
-path[0]=true
-path[n]=true
-console.log(path)
+path[0]=true;
+path[n]=true;
+console.log(path);
 //console.log("Leaving buildPath...")
-console.log("")
-return path
+console.log("");
+return path;
 }
 
 function stroll(path,index){
 //  console.log("Entering stroll...")
   if (index==(path.length-1)){
 //    console.log("index=path.length-1...return 1");
-    return 1
+    return 1;
   }
     else if (path[index]==false){
 //   console.log("path[index]=false...return 0");
-    return 0
+    return 0;
   }
     else{
 //     console.log("else...return stroll(path, index+1)");
 //     console.log("KEY..." + path[index])
-    return stroll(path, index+1)
+    return stroll(path, index+1);
   }
 }
 
@@ -47,20 +47,20 @@ function rsj (path, index){
 //console.log(index + " = " + path[index])
  if (index==(path.length-1)){
 //  console.log("index==path.lenght-1...return 1")
-    return 1
+    return 1;
     }
     else if (index>(path.length-1)){
 //   console.log("index>path.length-1...return 0")
-      return 0
+      return 0;
     }
   else if (path[index]==false){
   //    console.log("else if path[index] = false")
-      return 0
+      return 0;
    }
     else{
 //      console.log("else...return rsj recursive +1,+2,+5")
 //      console.log("KEY..." + path[index])
-      return rsj(path, index+1) + rsj(path, index+2) + rsj(path, index+5)
+      return rsj(path, index+1) + rsj(path, index+2) + rsj(path, index+5);
     }
   }
 
@@ -69,15 +69,15 @@ function rsj (path, index){
 //let testpath2 = buildPath(0.50,15);
 //console.log(testpath1)
 //console.log(testpath2);
-console.log("Stroll [T,F,T]")
+console.log("Stroll [T,F,T]");
 console.log(stroll([true,false,true],0));
 console.log("---------------------");
-console.log("Stroll [T,T,T]")
+console.log("Stroll [T,T,T]");
 console.log(stroll([true,true,true],0));
-console.log("---------------------")
-console.log("RSJ With Lenght 16 & 10% false probability")
+console.log("---------------------");
+console.log("RSJ With Lenght 16 & 10% false probability");
 console.log(rsj(buildPath(0.10,15),0));
 console.log("---------------------");
-console.log("RSJ With Lenght 16 & 50% false probability")
+console.log("RSJ With Lenght 16 & 50% false probability");
 console.log(rsj(buildPath(0.50,15),0));
-console.log("---------------------")
+console.log("---------------------");
